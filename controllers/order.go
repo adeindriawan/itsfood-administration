b192d3457ecc016bff3218f5b6dcfeaf238edc98
Ade Indriawan <aindriawan1@gmail.com>
2022-11-30 16:34:13 +0700

    Finalized vendor notification endpoint

--- controllers/order.go
+++ controllers/order.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"time"
+	"regexp"
+	"errors"
+	"strings"
 	"strconv"
 	"net/url"
 	"github.com/gin-gonic/gin"
@@ -40,7 +43,6 @@ type OrderDetailResult struct {
 
 func NotifyAVendorForAnOrder(c *gin.Context) {
 	var orderModel models.Order
-	var orderDetailModel models.OrderDetail
 	var orderDetailModels []models.OrderDetail
 	var order OrderResult
 	var orderDetails []OrderDetailResult
@@ -101,14 +103,27 @@ func NotifyAVendorForAnOrder(c *gin.Context) {
 		return
 	}
 
+	vendorPhone := orderDetails[0].VendorPhone
+	vendorPhoneNumber, err := sanitizePhoneNumber(vendorPhone)
+	if err != nil {
+		c.JSON(200, gin.H{
+			"status": "failed",
+			"result": nil,
+			"errors": err.Error(),
+			"description": "Gagal mengolah data nomor telepon vendor.",
+		})
+		return
+	}
+
 	// create message to vendor (whatsapp-enable link)
 	orderedAt := utils.ConvertDateToPhrase(order.CreatedAt, true)
 	orderedFor := utils.ConvertDateToPhrase(order.OrderedFor, true)
 	var details = ""
 	for _, item := range orderDetails {
+		var orderDetailModel models.OrderDetail
 		menuQty := strconv.Itoa(int(item.MenuQty))
 		details += item.MenuName + " " + menuQty + " porsi. Catatan: " + item.Note
-		services.DB.Where("id = ?", item.ID).First(&orderDetailModel)
+		services.DB.Where("id = ?", item.ID).First(&orderDetailModel) 
 		orderDetailDump := models.OrderDetailDump{
 			SourceID: item.ID,
 			OrderID: orderDetailModel.OrderID,
@@ -167,17 +182,43 @@ func NotifyAVendorForAnOrder(c *gin.Context) {
 	message := "Ada order untuk " + orderDetails[0].VendorName + " dengan ID #" + orderId + " dari " + order.CustomerName + " di " + order.CustomerUnit
 	message += " pada " + orderedAt + " untuk diantar pada " + orderedFor + " dengan rincian:\n"
 	message += details
-	// if all of the order details is sent, then update the order as ForwardedEntirely, otherwise as ForwardedPartially
-	// update the order details by the vendor as 'Sent'
+
+	whatsappAPI := "https://api.whatsapp.com/send/?phone="+ vendorPhoneNumber + "&text=" + url.QueryEscape(message)
+	whatsappAPI += "&type=phone_number&app_absent=0"
+	
 	c.JSON(200, gin.H{
 		"status": "success",
 		"result": map[string]interface{}{
 			"order": order,
 			"details": orderDetails,
-			"message": url.QueryEscape(message),
-			"ODmodel": orderDetailModel,
+			"messageLink": whatsappAPI,
 		},
 		"errors": nil,
 		"description": "Berhasil menyusun notifikasi untuk vendor untuk dikirim via Whatsapp.",
 	})
+}
+
+func sanitizePhoneNumber(number string) (string, error) {
+	var phoneNumber string = ""
+	var errNumberNotValid error = errors.New("nomor telepon tidak valid: nomor telepon tidak mengikuti standar nomor telepon 08xx/628xx")
+	var nonAlphanumericRegex = regexp.MustCompile(`\D+`)
+	phoneNumber += nonAlphanumericRegex.ReplaceAllString(number, "")
+
+	if len(phoneNumber) > 13 {
+		errNumberTooLong := errors.New("nomor telepon terlalu panjang: ada kemungkinan merupakan gabungan dari banyak nomor")
+		return "", errNumberTooLong
+	}
+
+	if phoneNumber[0:2] == "62" {
+		return phoneNumber, nil
+	}
+
+	if phoneNumber[0:2] == "08" {
+		prefix := "62"
+		phoneNumber = prefix + strings.TrimPrefix(phoneNumber, "0")
+
+		return phoneNumber, nil
+	}
+
+	return "", errNumberNotValid
 }
