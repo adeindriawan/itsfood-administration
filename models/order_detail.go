b9e8960fed6bd386377decd02478e7b8618dee11
zaqptutama <zaq.ptutama@gmail.com>
2023-03-13 10:31:35 +0700

    Add cost and discount prop on order_detail model.

--- models/order_detail.go
+++ models/order_detail.go
@@ -2,41 +2,44 @@ package models
 
 import (
 	"time"
+
 	"github.com/adeindriawan/itsfood-administration/services"
 )
 
 type OrderDetail struct {
-	ID uint64 					`gorm:"primaryKey" json:"id"`
-	OrderID uint64 			`gorm:"column:order_id;not null" json:"order_id"`
-	Order Order 				`json:"order"`
-	MenuID uint64 			`gorm:"column:menu_id;not null" json:"menu_id"`
-	Menu Menu						`json:"menu"`
-	Qty uint 						`gorm:"column:qty;not null" json:"qty"`
-	Price uint64 				`gorm:"column:price;not null" json:"price"`
-	COGS uint64 				`gorm:"column:cogs;not null" json:"cogs"`
-	Note string					`gorm:"column:note" json:"note"`
-	Status string 			`gorm:"column:status;not null" json:"status"`
-	CreatedAt time.Time `gorm:"column:created_at;not null" json:"created_at"`
-	UpdatedAt time.Time	`gorm:"column:updated_at" json:"updated_at"`
-	CreatedBy string 		`gorm:"column:created_by;not null" json:"created_by"`
+	ID        uint64     `gorm:"primaryKey" json:"id"`
+	OrderID   uint64     `gorm:"column:order_id;not null" json:"order_id"`
+	Order     Order      `json:"order"`
+	MenuID    uint64     `gorm:"column:menu_id;not null" json:"menu_id"`
+	Menu      Menu       `json:"menu"`
+	Qty       uint       `gorm:"column:qty;not null" json:"qty"`
+	Price     uint64     `gorm:"column:price;not null" json:"price"`
+	COGS      uint64     `gorm:"column:cogs;not null" json:"cogs"`
+	Note      string     `gorm:"column:note" json:"note"`
+	Status    string     `gorm:"column:status;not null" json:"status"`
+	Discounts []Discount `json:"discounts"`
+	Costs     []Cost     `json:"costs"`
+	CreatedAt time.Time  `gorm:"column:created_at;not null" json:"created_at"`
+	UpdatedAt time.Time  `gorm:"column:updated_at" json:"updated_at"`
+	CreatedBy string     `gorm:"column:created_by;not null" json:"created_by"`
 }
 
 type OrderDetailDump struct {
-	ID uint64 					`gorm:"primaryKey" json:"id"`
-	SourceID uint64			`gorm:"column:source_id;not null" json:"source_id"`
+	ID          uint64      `gorm:"primaryKey" json:"id"`
+	SourceID    uint64      `gorm:"column:source_id;not null" json:"source_id"`
 	OrderDetail OrderDetail `gorm:"foreignKey:SourceID" json:"order_detail"`
-	OrderID uint64 			`gorm:"column:order_id;not null" json:"order_id"`
-	Order Order 				`json:"order"`
-	MenuID uint64 			`gorm:"column:menu_id;not null" json:"menu_id"`
-	Menu Menu						`json:"menu"`
-	Qty uint 						`gorm:"column:qty;not null" json:"qty"`
-	Price uint64 				`gorm:"column:price;not null" json:"price"`
-	COGS uint64 				`gorm:"column:cogs;not null" json:"cogs"`
-	Note string					`gorm:"column:note" json:"note"`
-	Status string 			`gorm:"column:status;not null" json:"status"`
-	CreatedAt time.Time `gorm:"column:created_at;not null" json:"created_at"`
-	UpdatedAt time.Time	`gorm:"column:updated_at" json:"updated_at"`
-	CreatedBy string 		`gorm:"column:created_by;not null" json:"created_by"`
+	OrderID     uint64      `gorm:"column:order_id;not null" json:"order_id"`
+	Order       Order       `json:"order"`
+	MenuID      uint64      `gorm:"column:menu_id;not null" json:"menu_id"`
+	Menu        Menu        `json:"menu"`
+	Qty         uint        `gorm:"column:qty;not null" json:"qty"`
+	Price       uint64      `gorm:"column:price;not null" json:"price"`
+	COGS        uint64      `gorm:"column:cogs;not null" json:"cogs"`
+	Note        string      `gorm:"column:note" json:"note"`
+	Status      string      `gorm:"column:status;not null" json:"status"`
+	CreatedAt   time.Time   `gorm:"column:created_at;not null" json:"created_at"`
+	UpdatedAt   time.Time   `gorm:"column:updated_at" json:"updated_at"`
+	CreatedBy   string      `gorm:"column:created_by;not null" json:"created_by"`
 }
 
 func (OrderDetailDump) TableName() string {
@@ -49,14 +52,14 @@ func UpdateOrderDetail(params map[string]interface{}, update map[string]interfac
 
 	for _, item := range orderDetails {
 		orderDetailDump := OrderDetailDump{
-			SourceID: item.ID,
-			OrderID: item.OrderID,
-			MenuID: item.MenuID,
-			Qty: item.Qty,
-			Price: item.Price,
-			COGS: item.COGS,
-			Note: item.Note,
-			Status: item.Status,
+			SourceID:  item.ID,
+			OrderID:   item.OrderID,
+			MenuID:    item.MenuID,
+			Qty:       item.Qty,
+			Price:     item.Price,
+			COGS:      item.COGS,
+			Note:      item.Note,
+			Status:    item.Status,
 			CreatedAt: item.CreatedAt,
 			UpdatedAt: item.UpdatedAt,
 			CreatedBy: item.CreatedBy,
